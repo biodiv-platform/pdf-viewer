f82fd66ae4367a634b9daa60d44be17b6715f5dc
harshzalavadiya <harsh@strandls.com>
2021-09-02 23:29:39 +0530

    refactor: :recycle: url sanitize + cleanup

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-import { App } from "@tinyhttp/app"
-import https from "https"
-import http from "http"
-import { logger } from "@tinyhttp/logger"
-import sirv from "sirv"
+import { App } from "@tinyhttp/app";
+import { sanitizeUrl } from "@braintree/sanitize-url";
+import https from "https";
+import http from "http";
+import sirv from "sirv";
 
-const app = new App()
+const app = new App();
 
-app.use("/pdf-viewer/", sirv("pdf-viewer"))
+app.use("/pdf-viewer/", sirv("pdf-viewer"));
 
 app.get("/pdf-viewer/file", async (req, res) => {
-  const httpx = req.query.l.startsWith("https") ? https : http
+  const fPath = sanitizeUrl(req.query.l);
+  console.log(`fetching: ${fPath}`);
 
-  console.log(`fetching: ${req.query.l}`)
+  if (!fPath.startsWith("http")) {
+    res.status(403).end();
+    return;
+  }
+
+  const httpx = fPath.startsWith("https") ? https : http;
 
   return new Promise((resolve, reject) => {
     try {
-      httpx.get(req.query.l, async (stream) => {
+      httpx.get(fPath, async (stream) => {
         if (stream.headers["content-type"] !== "application/pdf") {
-          res.status(403).end()
-          resolve()
-          return
+          res.status(403).end();
+          resolve();
+          return;
         }
-        stream.pipe(res)
-        stream.on("end", resolve)
-      })
+        stream.pipe(res);
+        stream.on("end", resolve);
+      });
     } catch (e) {
-      console.error(e)
-      reject()
+      console.error(e);
+      reject();
     }
-  })
-})
+  });
+});
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
