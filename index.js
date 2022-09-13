101040dbad8b9dc599fac50ee7125c1157b8f04d
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2022-09-13 09:03:32 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ import sirv from "sirv";
 
 const app = new App();
 
+const getReqObject = (reqURL) => {
+  const fPath = sanitizeUrl(reqURL);
+  console.log(`fetching: ${fPath}`);
+
+  if (!fPath.startsWith("http")) return;
+
+  return {
+    fPath,
+    httpx: fPath.startsWith("https") ? request.https : request.http,
+  };
+};
+
 app.use("/pdf-viewer/", sirv("pdf-viewer"));
 
 app.get("/pdf-viewer/file", async (req, res) => {
-  const fPath = sanitizeUrl(req.query.l);
-  console.log(`fetching: ${fPath}`);
+  const { httpx, fPath } = getReqObject(req.query.l);
 
-  if (!fPath.startsWith("http")) {
-    res.status(403).end();
+  if (!httpx) {
+    res.send({ pdf: false });
     return;
   }
-
-  const httpx = fPath.startsWith("https") ? request.https : request.http;
 
   return new Promise((resolve, reject) => {
     try {
@@ -31,6 +40,28 @@ app.get("/pdf-viewer/file", async (req, res) => {
       });
     } catch (e) {
       console.error(e);
+      reject();
+    }
+  });
+});
+
+app.get("/pdf-viewer/validate", async (req, res) => {
+  const { httpx, fPath } = getReqObject(req.query.l);
+
+  if (!httpx) {
+    res.send({ pdf: false });
+    return;
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      httpx.get(fPath, async (stream) => {
+        res.send({ pdf: stream.headers["content-type"] === "application/pdf" });
+        resolve();
+      });
+    } catch (e) {
+      console.error(e);
+      res.send({ pdf: false });
       reject();
     }
   });
